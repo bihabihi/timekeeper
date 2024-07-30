4b08f452341dbd7807dc3e1bace092424c41173e
bihabihi <nabihahwajihah@gmail.com>
2024-07-30 18:41:00 +0800

    handle 404 & useeffect

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,10 @@
 const Error404 = () => {
     return (
-        <div>Wrong way, patah balik</div>
+        <div className="flex flex-col items-center justify-between">
+            <h1>404</h1>
+            <h3>Not found</h3>
+            <h5>The resource requested could not be found on this server</h5>
+        </div>
 )}
 
 export default Error404;
