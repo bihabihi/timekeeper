143f57d4c2854b6ba7364bfbc3e00680ec3e2c71
bihabihi <nabihahwajihah@gmail.com>
2024-07-30 18:47:43 +0800

    fix 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,11 @@
 const Error404 = () => {
     return (
-        <div className="flex flex-col items-center justify-between">
-            <h1>404</h1>
-            <h3>Not found</h3>
-            <h5>The resource requested could not be found on this server</h5>
+        <div className="h-screen flex flex-col items-center justify-center">
+            <div className="text-center">
+                <h1 className="text-8xl font-bold text-gray-800">404</h1>
+                <h3 className="text-4xl mt-2"> Not found</h3>
+                <h5 className="text-lg mt-2">The resource requested could not be found on this server</h5>
+            </div>
         </div>
 )}
 
